d5e82a72a018a90123d4a17a36b8d2777c21087b
Hieu <hieu.dang.dth_is_me@hcmut.edu.vn>
2021-07-10 11:06:17 +0700

    first commit 2 failures

---
+++ arithmetic_arranger/test.py
@@ -0,0 +1,73 @@
+
+def calculate_result(operator, op1, op2):
+  o1 = int(op1)
+  o2 = int(op2)
+  if (operator == '+'):
+    return o1 + o2
+  elif (operator == '-'):
+    return o1 - o2
+
+
+def arithmetic_arranger(problems, res=False):
+    arranged_problems = ""
+    first_line = []
+    second_line = []
+    third_line = []
+    res_line = []
+
+    if (len(problems) > 5):
+        return 'Error: Too many problems.'
+    else:
+        for i in range(len(problems)):
+            if (problems[i].find('*') != -1 or problems[i].find('/') != -1):
+                return "Error: Operator must be '+' or '-'."
+            operands = problems[i].split()
+        
+            if not (operands[0].isdigit() and operands[2].isdigit()):
+                return 'Error: Numbers must only contain digits.'
+            if (len(operands[0]) > 4 or len(operands[2]) > 4):
+                return 'Error: Numbers cannot be more than four digits.'
+            
+            max_len_per_prom = max(len(operands[0]), len(operands[2])) + 2
+            space_between_prom = 4
+
+            first_line.append((max_len_per_prom - len(operands[0])) * ' ' + operands[0])
+            second_line.append(operands[1] + (max_len_per_prom - 1 - len(operands[2])) * ' ' + operands[2])
+            third_line.append(max_len_per_prom * '-')
+            result = str(calculate_result(operands[1], operands[0], operands[2]))
+            res_line.append((max_len_per_prom - len(result)) * ' ' + result)
+
+    for line in first_line:
+        if (line != first_line[-1]):
+            arranged_problems = arranged_problems + line + space_between_prom * ' '
+        else:
+            arranged_problems = arranged_problems + line
+    arranged_problems = arranged_problems + '\n'
+    
+    for line in second_line:
+        if (line != first_line[-1]):
+            arranged_problems = arranged_problems + line + space_between_prom * ' '
+        else:
+            arranged_problems = arranged_problems + line
+    
+    arranged_problems = arranged_problems + '\n'    
+    for line in third_line:
+        if (line != first_line[-1]):
+            arranged_problems = arranged_problems + line + space_between_prom * ' '
+        else:
+            arranged_problems = arranged_problems + line
+    
+    if (res==True):
+        arranged_problems = arranged_problems + '\n'
+        for line in res_line:
+            if (line != first_line[-1]):
+                arranged_problems = arranged_problems + line + space_between_prom * ' '
+            else:
+                arranged_problems = arranged_problems + line
+
+    return arranged_problems
+
+print(len(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"])))
+
+expected = "    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----"
+print (len(expected))
